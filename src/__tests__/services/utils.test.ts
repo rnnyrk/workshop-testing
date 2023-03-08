f8c711cd70acba21314a6eaabe5f3a4356ab1f95
Jimmy Qian <jimmy.qian@labela.nl>
2023-03-08 15:46:19 +0100

    Improvements

--- src/__tests__/services/utils.test.ts
+++ src/__tests__/services/utils.test.ts
@@ -13,60 +13,58 @@ const ALPHA_INVALID = 'invalid';
 const ALPHA_TOO_HIGH = '1.2';
 const ALPHA_TOO_LOW = '-0.1';
 
-test('hexToRgba() - should return rgba value when the hex code is valid', () => {
-  const expected = 'rgba(130, 188, 0, 0.6)';
+describe('[HAPPY] - hexToRgba()', () => {
+  test('Should return rgba value when `hex` and `alpha` are valid', () => {
+    const expected = 'rgba(130, 188, 0, 0.6)';
 
-  expect(hexToRgba(HEX_CODE_VALID, ALPHA_VALID)).toBe(expected);
+    expect(hexToRgba(HEX_CODE_VALID, ALPHA_VALID)).toBe(expected);
+  });
 });
 
-test('hexToRgba() - should return null when the hex code is too long', () => {
-  const expected = null;
+describe('[SAD] - hexToRgba()', () => {
+  test('Should return null when the hex code is too long', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_TOO_LONG, ALPHA_VALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_TOO_LONG, ALPHA_VALID)).toBe(expected);
+  });
 
-test('hexToRgba() - should return null when the hex code is too short', () => {
-  const expected = null;
+  test('Should return null when the hex code is too short', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_TOO_SHORT, ALPHA_VALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_TOO_SHORT, ALPHA_VALID)).toBe(expected);
+  });
 
-// FAILED: Should we return null when a hex code without '#' is used?
-test('hexToRgba() - should return null when the hex code has no hashtag', () => {
-  const expected = null;
+  // FAILED: Should we return null when a hex code without '#' is used?
+  test('Should return null when the hex code has no hashtag', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_WITHOUT_HASHTAG, ALPHA_VALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_WITHOUT_HASHTAG, ALPHA_VALID)).toBe(expected);
+  });
 
-test('hexToRgba() - should return null when the hex code contains special characters', () => {
-  const expected = null;
+  test('Should return null when the hex code contains special characters', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_WITH_SPECIAL_CHARACTER, ALPHA_VALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_WITH_SPECIAL_CHARACTER, ALPHA_VALID)).toBe(expected);
+  });
 
-test('hexToRgba() - should return null when the `alpha` value is valid', () => {
-  const expected = 'rgba(130, 188, 0, 0.6)';
+  // FAILED: Should we return null when the alpha value is invalid?
+  test('Should return null when the `alpha` value is invalid', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_VALID, ALPHA_VALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_VALID, ALPHA_INVALID)).toBe(expected);
+  });
 
-// FAILED: Should we return null when the alpha value is invalid?
-test('hexToRgba() - should return null when the `alpha` value is invalid', () => {
-  const expected = null;
+  // FAILED: Should we return null when the alpha value is out of range [0, 1]?
+  test('Should return null when the `alpha` value is too high', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_VALID, ALPHA_INVALID)).toBe(expected);
-});
+    expect(hexToRgba(HEX_CODE_VALID, ALPHA_TOO_HIGH)).toBe(expected);
+  });
 
-// FAILED: Should we return null when the alpha value is out of range [0, 1]?
-test('hexToRgba() - should return null when the `alpha` value is too high', () => {
-  const expected = null;
+  // FAILED: Should we return null when the alpha value is out of range [0, 1]?
+  test('Should return null when the `alpha` value is too low', () => {
+    const expected = null;
 
-  expect(hexToRgba(HEX_CODE_VALID, ALPHA_TOO_HIGH)).toBe(expected);
-});
-
-// FAILED: Should we return null when the alpha value is out of range [0, 1]?
-test('hexToRgba() - should return null when the `alpha` value is too low', () => {
-  const expected = null;
-
-  expect(hexToRgba(HEX_CODE_VALID, ALPHA_TOO_LOW)).toBe(expected);
+    expect(hexToRgba(HEX_CODE_VALID, ALPHA_TOO_LOW)).toBe(expected);
+  });
 });
